21ada2352576461bfbcc55429cf26fa3de100c30
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2025-01-04 23:07:59 +0545

    Shifting Operations Implemenations Complete

--- Ciphers/Symmetric Ciphers/DES.c
+++ Ciphers/Symmetric Ciphers/DES.c
@@ -60,10 +60,11 @@ void messageSplitter(char *input, char *output1, char *output2) {
 void messageMerger(char *input1, char *input2, char *output) {
     strcpy(output, input1);
     strcat(output, input2);
+    output[strlen(input1)+strlen(input2)]='\0';
 }
 
 void leftShiftCircular(char *input, int times) {
-    for (int i=0; i<times; i++) {
+    for (int j=0; j<times; j++) {
         char hold = input[0];
         for (int i=0; i < strlen(input)-1; i++) {
             input[i] = input[i+1];
@@ -75,6 +76,22 @@ void leftShiftCircular(char *input, int times) {
 void leftShiftCircularDual(char *input1, char *input2, int times) {
     leftShiftCircular(input1, times);
     leftShiftCircular(input2,times);
+}
+
+void scheduledLeftShifts(char C[ROUNDS][29], char D[ROUNDS][29]) {
+    for (int i=1; i <= ROUNDS; i++) {
+        strcpy(C[i], C[i-1]); 
+        strcpy(D[i], D[i-1]);
+        leftShiftCircularDual(C[i], D[i],rotationSchedule[i-1]);
+        printf("\n C%-2d: %s\n D%-2d: %s",i, C[i],i, D[i]);
+    }
+}
+
+void keyGenerator(char C[ROUNDS][29], char D[ROUNDS][29],char keys[ROUNDS][57]) {
+    for (int i=1; i <= ROUNDS; i++) {
+        messageMerger(C[i], D[i], keys[i-1]);
+        printf("\n ShiftedMerged[%-2d]: %s",i, keys[i-1]);
+    }
 }
 
 int main() {
@@ -97,14 +114,11 @@ int main() {
     printf("K+=");
     stringPrinter(binaryKeyplus->msg, 7);
 
-    char C[ROUNDS][28], D[ROUNDS][28], keys[ROUNDS][56], entire[56];
+    char C[ROUNDS][29], D[ROUNDS][29], keys[ROUNDS][57], entire[57];
     messageSplitter(binaryKeyplus->msg, C[0], D[0]);
-    printf("\n C= %s \n D= %s \n", C[0], D[0]);
-    messageMerger(C[0], D[0], entire);
-    printf("\n merged: %s", entire);
+    printf("\n C0= %s \n D0= %s", C[0], D[0]);
 
-    leftShiftCircularDual(C[0], D[0],2);
-    printf("\n Cleft: %s\n Dleft: %s", C[0], D[0]);
-
+    scheduledLeftShifts(C, D);
+    keyGenerator(C, D, keys);
     return 0;
 }
