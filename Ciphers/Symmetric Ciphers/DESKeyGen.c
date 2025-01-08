7955caa6a4ed22c744a3438daaf30f0d2a63470a
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2025-01-08 19:23:59 +0545

    Major Cleanups and Interfacing
    - Cleaned up code and added Step wise listing for ease of comparison with algorithm
    - Converted the standalone to an interface type to be later used in encoding

--- Ciphers/Symmetric Ciphers/DESKeyGen.c
+++ Ciphers/Symmetric Ciphers/DESKeyGen.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../Modules/DESModules.c"
 #include "../Modules/DESKeyGenData.c"
-
-#define SPLITLENGTH 28
-#define PERMUTEDLENGTH 56
 
 void keyPermuteMatrixInitializer(permuteMatrix *PC1M, permuteMatrix *PC2M) {
     PC1M->Column = 7; PC1M->Row = 8;
@@ -15,34 +8,7 @@ void keyPermuteMatrixInitializer(permuteMatrix *PC1M, permuteMatrix *PC2M) {
     PCCopier(PC2M, PC2);
 }
 
-void primaryKeyAppender(char *output, char *binaryGroup) {
-    if (output == NULL || binaryGroup == NULL) return;
-    int pointer = strlen(output);
-    for (int i = 0; i < 4; i++) {
-        output[pointer++] = binaryGroup[i];
-    }
-    output[pointer] = 0; 
-}
-
-void plainTextToMessage(char *output, char *plainText) {
-    strcpy(output, "");
-    for (int i = 0; i < strlen(plainText); i++) {
-        primaryKeyAppender(output, hexToBinGroup(plainText[i]));
-    }
-}
-
-void permutedString(char *input, char *output, permuteMatrix PM) {
-    int pointer =0;
-    for (int i = 0; i < PM.Row; i++) {
-        for (int j = 0; j < PM.Column; j++) {
-            output[pointer] = input[PM.PC[i][j]-1];
-            pointer++;
-        }
-    }
-    output[pointer]=0;
-}
-
-void messageSplitter(char *input, char *output1, char *output2) {
+void messageHalfSplitter(char *input, char *output1, char *output2) {
     if (strlen(input)%2==0) {
         for (int i=0; i < strlen(input); i++) {
             if (i < strlen(input)/2) {
@@ -58,7 +24,7 @@ void messageSplitter(char *input, char *output1, char *output2) {
     output2[strlen(input)/2]=0;
 }
 
-void messageMerger(char *input1, char *input2, char *output) {
+void messageHalvesMerger(char *input1, char *input2, char *output) {
     strcpy(output, input1);
     strcat(output, input2);
     output[strlen(input1)+strlen(input2)]='\0';
@@ -79,7 +45,7 @@ void leftShiftCircularDual(char *input1, char *input2, int times) {
     leftShiftCircular(input2,times);
 }
 
-void scheduledLeftShifts(char C[ROUNDS+1][SPLITLENGTH+1], char D[ROUNDS+1][SPLITLENGTH+1], int verbose) {
+void scheduledLeftShifts(char C[ROUNDS+1][SPLITKEYLENGTH+1], char D[ROUNDS+1][SPLITKEYLENGTH+1], int verbose) {
     // Print the First Key that does not require permuting
         if (verbose) {
             printf("\n C0 : %s \n D0 : %s", C[0], D[0]);
@@ -102,33 +68,33 @@ void scheduledLeftShifts(char C[ROUNDS+1][SPLITLENGTH+1], char D[ROUNDS+1][SPLIT
         verbose? printf("\n"): (void)0;
 }
 
-void permuteShiftedKeys(char C[ROUNDS+1][SPLITLENGTH+1], char D[ROUNDS+1][SPLITLENGTH+1],char keys[ROUNDS+1][PERMUTEDLENGTH+1], permuteMatrix *PC2M, int verbose) {
+void permuteShiftedKeys(char C[ROUNDS+1][SPLITKEYLENGTH+1], char D[ROUNDS+1][SPLITKEYLENGTH+1],char keys[ROUNDS+1][PERMUTEDLENGTH+1], permuteMatrix *PC2M, int verbose, int keysVerbose) {
     char temp[ROUNDS+1][PERMUTEDLENGTH+1];
     for (int i=1; i <= ROUNDS; i++) {
-        messageMerger(C[i], D[i], keys[i-1]);
+        messageHalvesMerger(C[i], D[i], keys[i-1]);
 
         permutedString(keys[i-1], temp[i-1], *PC2M);
 
         strcpy(keys[i-1], temp[i-1]);
 
-        //if (verbose) {
+        if (keysVerbose) {
             printf("Keys[%02d]:",i);
             stringPrinter(keys[i-1], 6);
             printf("\n");
-        //}
+        }
     }
 }
 
-void keyGenerator(char keysOutput[ROUNDS+1][PERMUTEDLENGTH+1], int verbose) {
+void keyGenerator(char keysOutput[ROUNDS+1][PERMUTEDLENGTH+1], int verbose, int keysVerbose) {
     permuteMatrix PC1M, PC2M;
-    char C[ROUNDS+1][SPLITLENGTH+1], D[ROUNDS+1][SPLITLENGTH+1];
-    char *binaryKey=malloc((MAXKEYLENGTH+1) * sizeof(char)), *binaryKeyplus=malloc(MAXKEYLENGTH+1 * sizeof(char));
+    char C[ROUNDS+1][SPLITKEYLENGTH+1], D[ROUNDS+1][SPLITKEYLENGTH+1];
+    char *binaryKey=malloc((MAXKEYLENGTH+1) * sizeof(char)), *binaryKeyPlus=malloc(MAXKEYLENGTH+1 * sizeof(char));
 
     // 🌿 Initialize the Arrays into the Data Structure
     keyPermuteMatrixInitializer(&PC1M, &PC2M);
 
     // 🌿 Convert the Hexadecimal Key to Binary Key Result=64bit
-    plainTextToMessage(binaryKey, keyWord);
+    plainTextToMessage(keyWord, binaryKey);
 
     // Print the key
         if (verbose) {
@@ -137,32 +103,24 @@ void keyGenerator(char keysOutput[ROUNDS+1][PERMUTEDLENGTH+1], int verbose) {
         }
     
     // 🌿 Permute the Key against PC1 Result=56bit
-    permutedString(binaryKey, binaryKeyplus, PC1M);
+    permutedString(binaryKey, binaryKeyPlus, PC1M);
 
     // Print the permuted Key
         if (verbose) {
             printf("K+=");
-            stringPrinter(binaryKeyplus, PC1M.Column);
+            stringPrinter(binaryKeyPlus, PC1M.Column);
         }
 
     // 🌿 Divide the Key to C and D havles Result=28 bit each
-    messageSplitter(binaryKeyplus, C[0], D[0]);
+    messageHalfSplitter(binaryKeyPlus, C[0], D[0]);
 
     // 🌿 Individually Circular Left shift on C and D
     scheduledLeftShifts(C, D, verbose);
 
     // 🌿 Merge and Permute the key finally against PC2 Res=48 bit
-    permuteShiftedKeys(C, D, keysOutput, &PC2M, verbose);
+    permuteShiftedKeys(C, D, keysOutput, &PC2M, verbose, keysVerbose);
 
     // Freeing 
     free(binaryKey);
-    free(binaryKeyplus);
-}
-
-int main() {
-    int verbose =0;
-    char keys[ROUNDS+1][PERMUTEDLENGTH+1];
-    
-    keyGenerator(keys,verbose); 
-    return 0;
+    free(binaryKeyPlus);
 }
